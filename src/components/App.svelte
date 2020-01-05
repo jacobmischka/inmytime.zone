ee7449bd73722ed65ab3bfb9d86b740406103de6
Jacob Mischka <jacob@mischka.me>
2020-01-05 05:45:39 -0600

    Add a few small niceties/qol improvements

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,4 +1,6 @@
 <header>
+	<h1>inmytime.zone</h1>
+
 	{#if date}
 		<nav>
 			<a href="/" on:click={handleClear}>Home</a>
@@ -22,14 +24,32 @@
 	{/if}
 
 	<Create />
-</main>
 
-<footer>
-</footer>
+	{#if !date}
+		<aside>
+			<p>
+				Tip: type variations of the following into your address bar
+				to easily create full links that you can send to others:
+			</p>
+
+			<ul>
+				{#each examples as example}
+					<li>
+						<a href={`//${example}.inmytime.zone`}>{example}.inmytime.zone</a>
+					</li>
+				{/each}
+			</ul>
+		</aside>
+	{/if}
+</main>
 
 <svelte:window on:popstate={getDate} />
 
 <style>
+	h1 {
+		margin: 0;
+	}
+
 	main {
 		font-size: 1.25em;
 		display: flex;
@@ -38,8 +58,16 @@
 		align-items: center;
 	}
 
-	div {
-		padding: 2em;
+	aside {
+		text-align: center;
+	}
+
+	aside ul {
+		padding: 0;
+	}
+
+	aside li {
+		list-style: none;
 	}
 </style>
 
@@ -51,9 +79,26 @@
 
 	const subRe = /(\d+)(am|pm|AM|PM)?/;
 
+	const examples = [
+		'10',
+		'930',
+		'12pm',
+		'1034pm'
+	];
+
+
 	let date;
 
-	inspectSubdomain();
+
+	const hasSubdomain = (
+		window.location.hostname.indexOf('.')
+		!== window.location.hostname.lastIndexOf('.')
+	);
+
+	if (hasSubdomain) {
+		inspectSubdomain();
+	}
+
 	getDate();
 
 
@@ -72,38 +117,36 @@
 	function inspectSubdomain() {
 		try {
 			const { hostname, origin } = window.location;
+			let subdomain = hostname.substring(0, hostname.indexOf('.'));
 
-			const hasSubdomain = hostname.indexOf('.') !== hostname.lastIndexOf('.');
-			if (hasSubdomain) {
-				let subdomain = hostname.substring(0, hostname.indexOf('.'));
+			if (subdomain !== 'www') {
+				const matches = subdomain.match(subRe);
+				const time = matches[1];
+				let hour = 0, minute = 0;
 
-				if (subdomain !== 'www') {
-					const matches = subdomain.match(subRe);
-					const time = matches[1];
-					let hour = 0, minute = 0;
-
-					if (time.length <= 2) {
-						hour = Number(time);
-					} else {
-						const i = time.length - 2;
-						hour = Number(time.substring(0, i));
-						minute = Number(time.substring(i));
-					}
-
-					if (hour <= 12 && matches.length > 2) {
-						const ampm = matches[2];
-						if (ampm && ampm.toLowerCase() === 'pm') {
-							hour += 12;
-						}
-					}
-
-					const d = DateTime.local().set({ hour, minute });
-					const domain = origin.replace(`${subdomain}.`, '');
-					console.log(domain);
-					const params = new URLSearchParams();
-					params.set('iso', d.toISO());
-					window.location =  `${domain}?${params.toString()}`;
+				if (time.length <= 2) {
+					hour = Number(time);
+				} else {
+					const i = time.length - 2;
+					hour = Number(time.substring(0, i));
+					minute = Number(time.substring(i));
 				}
+
+				if (matches.length > 2 && matches[2]) {
+					const ampm = matches[2].toLowerCase();
+					if (hour < 12 && ampm === 'pm') {
+						hour += 12;
+					} else if (hour === 12 && ampm === 'am') {
+						hour = 0;
+					}
+				}
+
+				const d = DateTime.local().set({ hour, minute });
+				const domain = origin.replace(`${subdomain}.`, '');
+				console.log(domain);
+				const params = new URLSearchParams();
+				params.set('iso', d.toISO());
+				window.location =  `${domain}?${params.toString()}`;
 			}
 		} catch (e) {
 			console.error(e);
--- src/components/Create.svelte
+++ src/components/Create.svelte
@@ -12,18 +12,46 @@
 </form>
 
 <style>
-	form {
+	h2 {
+		font-size: 1.25em;
 	}
 </style>
 
 <script>
 	import Flatpickr from 'svelte-flatpickr';
 
+	const MINUTE_ROUND_TIME = 15;
+
 	let date = '';
+
+	let defaultHour, defaultMinute;
+
+	try {
+		const now = new Date();
+		defaultHour = now.getHours();
+		defaultMinute = now.getMinutes() + MINUTE_ROUND_TIME;
+		defaultMinute -= defaultMinute % MINUTE_ROUND_TIME;
+
+		if (defaultMinute > 60) {
+			defaultMinute -= 60;
+			defaultHour += 1;
+		}
+
+		if (defaultHour > 24) {
+			defaultHour = undefined;
+			defaultMinute = undefined;
+		}
+	} catch (e) {
+		console.error(e);
+	}
+
+
 
 	const flatpickrOptions = {
 		enableTime: true,
-		dateFormat: 'F j, Y h:i K'
+		dateFormat: 'F j, Y h:i K',
+		defaultHour,
+		defaultMinute,
 	};
 
 	function handleSubmit(event) {
